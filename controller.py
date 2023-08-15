92e960fa18dce710d1e8692911deec8cf862fe4f
piratejas <piratejas@gmail.com>
2023-08-15 18:19:21 +0100

    sort by column implemented - need to fix

--- controller.py
+++ controller.py
@@ -8,15 +8,17 @@ main = Blueprint("main", __name__)
 @main.route('/', methods=["GET", "POST"])
 def index():
     headers = {"Content-Type": "application/json", "X-API-Key": "THIS_IS_THE_API_KEY"}
+    sort = request.args.get("sort", "")
+    
     offset = int(request.args.get("offset", 0))
     limit = int(request.args.get("limit", 0))
     if request.method == "POST":
-        limit = int(request.form["limit"])
-    url = f"http://localhost:8080/api/bids?offset={offset}&limit={limit}"
+        limit = 5 if not request.form.get("limit") else int(request.form["limit"])
+    url = f"http://localhost:8080/api/bids?sort={sort}&offset={offset}&limit={limit}"
     response = requests.get(url, headers=headers)
     if response.status_code == 200:
         bids = response.json()["items"]
-        return render_template('index.html', bids=bids, limit=limit, offset=offset)
+        return render_template('index.html', bids=bids, limit=limit, offset=offset, sort=sort)
     else:
         return render_template('error.html', error=response.json(), status=response.status_code)
         
