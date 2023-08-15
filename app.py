e1797effda965bdaf539672c09c2412de8f88d85
piratejas <piratejas@gmail.com>
2023-08-15 16:22:11 +0100

    pagination and set limit/offset

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ app = Flask(__name__, template_folder="views")
 app.register_blueprint(main)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=3000)
--- controller.py
+++ controller.py
@@ -4,13 +4,24 @@ from flask import Blueprint, render_template, request
 
 main = Blueprint("main", __name__)
 
-# View
-@main.route('/')
+
+@main.route('/', methods=["GET", "POST"])
 def index():
     headers = {"Content-Type": "application/json", "X-API-Key": "THIS_IS_THE_API_KEY"}
-    response = requests.get("http://localhost:8080/api/bids", headers=headers)
-    bids = response.json()["items"]
-    return render_template('index.html', bids=bids)
+    offset = int(request.args.get("offset", 0))
+    limit = int(request.args.get("limit", 0))
+    if request.method == "POST":
+        limit = int(request.form["limit"])
+    last_offset = offset - limit
+    next_offset = offset + limit
+    url = f"http://localhost:8080/api/bids?offset={offset}&limit={limit}"
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        bids = response.json()["items"]
+        return render_template('index.html', bids=bids, limit=limit, offset=offset, next_offset=next_offset, last_offset=last_offset)
+    else:
+        return render_template('error.html', error=response.json(), status=response.status_code)
+        
 
 # # Controller
 # @app.route('/add_bid', methods=['POST'])
