bb860b56bc8f7c2f6bf107032de03717d65abf3f
piratejas <piratejas@gmail.com>
2023-08-16 16:41:34 +0100

    added post bid

--- controller.py
+++ controller.py
@@ -1,5 +1,6 @@
-import requests
+import json, requests
 from flask import Blueprint, render_template, request
+from model import Bid
 
 
 main = Blueprint("main", __name__)
@@ -18,21 +19,32 @@ def index():
     response = requests.get(url, headers=headers)
     if response.status_code == 200:
         bids = response.json()["items"]
-        return render_template('index.html', bids=bids, limit=limit, offset=offset, sort=sort)
+        total = response.json()["total_count"]
+        return render_template('index.html', bids=bids, total=total, limit=limit, offset=offset, sort=sort)
     else:
         message = response.json()["Error"]
         status=response.status_code
         return render_template('error.html', message=message, status=status)
         
 
-# # Controller
-# @app.route('/add_bid', methods=['POST'])
-# def add_bid():
-#     title = request.form['title']
-#     author = request.form['author']
-#     book = Book(title, author)
-#     # ...
-#     # Do something with the book object,
-#     # like adding it to a database.
-#     # ...
-#     return render_template('success.html', book=book)
+# Controller
+@main.route('/add_bid', methods=['POST'])
+def add_bid():
+    tender = request.form["tender"]
+    bid_date = f'{request.form["year"]}-{request.form["month"]}-{request.form["day"]}'
+    client = request.form["client"]
+    alias = request.form["alias"]
+    bid_folder_url = request.form["bid_folder_url"]
+    was_successful = request.form["was_successful"]
+    data = Bid(tender=tender, bid_date=bid_date, client=client, alias=alias, bid_folder_url=bid_folder_url, was_successful=was_successful)
+    url = "http://localhost:8080/api/bids"
+    headers = {"Content-Type": "application/json", "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IlRlc3RlciBNY1Rlc3RmYWNlIiwiYWRtaW4iOmZhbHNlfQ.Dg7f8LVtALYWvjZH31re5C-Pc6Hp6Ra-U4LAy0ZQQ9M"}
+    response = requests.post(url, headers=headers, data=json.dumps(data.__dict__))
+    if response.status_code == 201:
+        status=response.status_code
+        bid = response.json()
+        return render_template('success.html', bid=bid, status=status)
+    else:
+        message = response.json()["Error"]
+        status=response.status_code
+        return render_template('error.html', message=message, status=status)
--- model.py
+++ model.py
@@ -9,7 +9,12 @@ import requests
 #         self.author = author
 
 class Bid:
-    def __init__(self):
-        pass
+    def __init__(self,tender, bid_date, client, alias=None, bid_folder_url=None, was_successful=None):
+        self.tender = tender
+        self.bid_date = bid_date
+        self.client = client
+        self.alias = alias
+        self.bid_folder_url = bid_folder_url
+        self.was_successful = was_successful
     def get_all(self):
         pass
