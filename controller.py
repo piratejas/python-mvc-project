d0eb56e88a8454c389febb89ec5684bed6d0d1ff
piratejas <piratejas@gmail.com>
2023-08-22 12:33:18 +0100

    moved hard-coded example secrets to .env

--- controller.py
+++ controller.py
@@ -1,4 +1,4 @@
-import json, requests
+import json, os, requests
 from flask import Blueprint, render_template, request
 from model import Bid
 
@@ -8,7 +8,7 @@ main = Blueprint("main", __name__)
 
 @main.route("/", methods=["GET", "POST"])
 def index():
-    headers = {"Content-Type": "application/json", "X-API-Key": "THIS_IS_THE_API_KEY"}
+    headers = {"Content-Type": "application/json", "X-API-Key": os.getenv("API_KEY")}
     sort = request.args.get("sort", "")
     offset = int(request.args.get("offset", 0))
     limit = int(request.args.get("limit", 5))
@@ -34,24 +34,19 @@ def index():
 # Controller
 @main.route("/add_bid", methods=["POST"])
 def add_bid():
-    tender = request.form["tender"]
-    bid_date = f'{request.form["year"]}-{request.form["month"]}-{request.form["day"]}'
-    client = request.form["client"]
-    alias = request.form["alias"]
-    bid_folder_url = request.form["bid_folder_url"]
-    was_successful = request.form["was_successful"]
     data = Bid(
-        tender=tender,
-        bid_date=bid_date,
-        client=client,
-        alias=alias,
-        bid_folder_url=bid_folder_url,
-        was_successful=was_successful,
+        tender=request.form["tender"],
+        bid_date=f'{request.form["year"]}-{request.form["month"]}-{request.form["day"]}',
+        client=request.form["client"],
+        alias=request.form["alias"],
+        bid_folder_url=request.form["bid_folder_url"],
+        was_successful=request.form["was_successful"],
+        feedback = {"description": request.form["feedback_description"], "url": request.form["feedback_url"]}
     )
     url = "http://localhost:8080/api/bids"
     headers = {
         "Content-Type": "application/json",
-        "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IlRlc3RlciBNY1Rlc3RmYWNlIiwiYWRtaW4iOmZhbHNlfQ.Dg7f8LVtALYWvjZH31re5C-Pc6Hp6Ra-U4LAy0ZQQ9M",
+        "Authorization": f'Bearer {os.getenv("TOKEN")}',
     }
     response = requests.post(url, headers=headers, data=json.dumps(data.__dict__))
     if response.status_code == 201:
--- model.py
+++ model.py
@@ -9,12 +9,13 @@ import requests
 #         self.author = author
 
 class Bid:
-    def __init__(self,tender, bid_date, client, alias=None, bid_folder_url=None, was_successful=None):
+    def __init__(self,tender, bid_date, client, alias=None, bid_folder_url=None, was_successful=None, feedback=None):
         self.tender = tender
         self.bid_date = bid_date
         self.client = client
         self.alias = alias
         self.bid_folder_url = bid_folder_url
         self.was_successful = was_successful
+        self.feedback = feedback
     def get_all(self):
         pass
