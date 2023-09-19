d23fc15f7b89c3664577b5481a77594907ff6b78
piratejas <piratejas@gmail.com>
2023-09-19 15:44:54 +0100

    refactor: moved business logic into model as class method

--- controller.py
+++ controller.py
@@ -1,4 +1,3 @@
-import json, os, requests
 from flask import Blueprint, render_template, request
 from model import Bid
 
@@ -8,17 +7,12 @@ main = Blueprint("main", __name__)
 
 @main.route("/", methods=["GET", "POST"])
 def index():
-    headers = {"Content-Type": "application/json", "X-API-Key": os.getenv("API_KEY")}
     sort = request.args.get("sort", "")
     offset = int(request.args.get("offset", 0))
     limit = int(request.args.get("limit", 5))
-    if request.method == "POST":
-        limit = (
-            limit if not request.form.get("limit") else int(request.form.get("limit"))
-        )
-        sort = request.form.get("sort", sort)
-    url = f"http://localhost:8080/api/bids?sort={sort}&offset={offset}&limit={limit}"
-    response = requests.get(url, headers=headers)
+
+    response = Bid.get_all(sort=sort, offset=offset, limit=limit)
+    
     if response.status_code == 200:
         bids = response.json()["items"]
         total = response.json()["total_count"]
@@ -29,31 +23,32 @@ def index():
         message = response.json()["Error"]
         status = response.status_code
         return render_template("error.html", message=message, status=status)
+    
 
 
 # Controller
-@main.route("/add_bid", methods=["POST"])
-def add_bid():
-    data = Bid(
-        tender=request.form["tender"],
-        bid_date=f'{request.form["year"]}-{request.form["month"]}-{request.form["day"]}',
-        client=request.form["client"],
-        alias=request.form["alias"],
-        bid_folder_url=request.form["bid_folder_url"],
-        was_successful=request.form["was_successful"],
-        feedback = {"description": request.form["feedback_description"], "url": request.form["feedback_url"]}
-    )
-    url = "http://localhost:8080/api/bids"
-    headers = {
-        "Content-Type": "application/json",
-        "Authorization": f'Bearer {os.getenv("TOKEN")}',
-    }
-    response = requests.post(url, headers=headers, data=json.dumps(data.__dict__))
-    if response.status_code == 201:
-        status = response.status_code
-        bid = response.json()
-        return render_template("success.html", bid=bid, status=status)
-    else:
-        message = response.json()["Error"]
-        status = response.status_code
-        return render_template("error.html", message=message, status=status)
+# @main.route("/add_bid", methods=["POST"])
+# def add_bid():
+#     data = Bid(
+#         tender=request.form["tender"],
+#         bid_date=f'{request.form["year"]}-{request.form["month"]}-{request.form["day"]}',
+#         client=request.form["client"],
+#         alias=request.form["alias"],
+#         bid_folder_url=request.form["bid_folder_url"],
+#         was_successful=request.form["was_successful"],
+#         feedback = {"description": request.form["feedback_description"], "url": request.form["feedback_url"]}
+#     )
+#     url = "http://localhost:8080/api/bids"
+#     headers = {
+#         "Content-Type": "application/json",
+#         "Authorization": f'Bearer {os.getenv("TOKEN")}',
+#     }
+#     response = requests.post(url, headers=headers, data=json.dumps(data.__dict__))
+#     if response.status_code == 201:
+#         status = response.status_code
+#         bid = response.json()
+#         return render_template("success.html", bid=bid, status=status)
+#     else:
+#         message = response.json()["Error"]
+#         status = response.status_code
+#         return render_template("error.html", message=message, status=status)
--- model.py
+++ model.py
@@ -1,14 +1,8 @@
-import requests
-
-# # Model
-# class Book:
-#     # Instead of a simple class,
-#     # a database model would more likely to be used.
-#     def __init__(self, title, author):
-#         self.title = title
-#         self.author = author
+import os, requests
+from flask import request
 
 class Bid:
+
     def __init__(self,tender, bid_date, client, alias=None, bid_folder_url=None, was_successful=None, feedback=None):
         self.tender = tender
         self.bid_date = bid_date
@@ -17,5 +11,14 @@ class Bid:
         self.bid_folder_url = bid_folder_url
         self.was_successful = was_successful
         self.feedback = feedback
-    def get_all(self):
-        pass
+    
+    @classmethod
+    def get_all(cls, sort, offset, limit):
+        headers = {"Content-Type": "application/json", "X-API-Key": os.getenv("API_KEY")}
+        if request.method == "POST":
+            limit = int(request.form.get("limit", limit))
+            sort = request.form.get("sort", sort)
+        url = f"http://localhost:8080/api/bids?sort={sort}&offset={offset}&limit={limit}"
+        response = requests.get(url, headers=headers)
+
+        return response
